53884d4b9a57091f3bd6e6951db445a79a71ebf9
Andrii <andrii@testing4success.com>
2021-08-31 16:26:31 -0400

    move to git react native bare app

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
+import 'react-native-gesture-handler';
+
+import * as Font from 'expo-font';
+
+import React, { useState } from 'react';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+
+import AppLoading from 'expo-app-loading';
+import MainDrawerNavi from './navigation/MainDrawerNavi';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import authReducer from './store/reducers/auth';
+import cartReducer from './store/reducers/cart';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ordersReducer from './store/reducers/order'
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk), composeWithDevTools());
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+      <MainDrawerNavi />
       <StatusBar style="auto" />
-    </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
